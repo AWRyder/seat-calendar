787a6b14d872a4eec7e9eae7cfd432b7c4dcd71a
João Braz <joao.braz@b-i.com>
2017-12-30 23:33:35 +0000

    pings every 24 h

--- src/Commands/RemindOperation.php
+++ src/Commands/RemindOperation.php
@@ -33,7 +33,7 @@ class RemindOperation extends Command
 			foreach($ops as $op) 
 			{
 				if ($op->status == 'incoming') {
-					if(in_array($now->diffInMinutes($op->start_at, false), $this->marks) || $now->diffInMinutes($op->start_at, false) % 720 == 0 )
+					if(in_array($now->diffInMinutes($op->start_at, false), $this->marks) || $now->diffInMinutes($op->start_at, false) % 1440 == 0 )
 					{
 						Notification::send($op, new OperationPinged());
 					}
